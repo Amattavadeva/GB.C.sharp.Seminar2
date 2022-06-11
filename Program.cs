6d14088a3a0a4d9584bd5cb7c41d8152f4e91d9b
Alex QAsper <semidog@gmail.com>
2022-06-11 12:11:45 +0300

    hmwrk 2 done

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 10: Напишите программу, которая принимает на
+вход трёхзначное число и на выходе показывает вторую
+цифру этого числа.
+
+int n = int.Parse(Console.ReadLine());
+int num = (n % 100 ) / 10;
+
+Console.WriteLine(num);
+ */
+
+int n = int.Parse(Console.ReadLine());
+
+if (n > 5)
+{
+    Console.WriteLine("да");
+}
+else{
+    Console.WriteLine("нет");
+}
